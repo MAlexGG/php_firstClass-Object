b2b75526e7b3528b0373a6ba77636aefd84f2243
MAlexGG <alexandragalarzag@gmail.com>
2021-04-28 10:52:21 +0200

    first commit

---
+++ template.php
@@ -0,0 +1,53 @@
+<?php
+
+class card {
+    public $img;
+    public $title;
+    public $txt;
+    public $template;
+
+    public function __construct($img, $title, $txt)
+    {
+        $this->img = $img;
+        $this->title = $title;
+        $this->txt = $txt;
+    }
+    public function render(){
+        $this->template =   <<<TAG
+    
+        <div class="col-lg-4 mb-4">
+            <div class="card">
+                <img style="width:350px; height:300px"; object-fit:cover" src= {$this->img}>
+                <div class="card-body">
+                    <h5 class="card-title">{$this->title}</h5>
+                    <p class="card-text">{$this->txt}</p>
+                    <a href="" class="btn btn-outline-success btn-sm">Read More</a>
+                </div>
+            </div>
+        </div>
+     
+      TAG;
+    }
+
+    public function print(){
+         echo $this->template;
+    }
+}
+
+$sunset = new card("https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60", "Sunset", "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut eum similique repellat a laborum, rerum voluptates ipsam eos quo tempore iusto dolore modi dolorum in pariatur. Incidunt repellendus praesentium quae!");
+
+echo $sunset->render();
+echo $sunset->print();
+
+$forest = new card("https://images.unsplash.com/photo-1516214104703-d870798883c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60", "Forest", "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut eum similique repellat a laborum, rerum voluptates ipsam eos quo tempore iusto dolore modi dolorum in pariatur. Incidunt repellendus praesentium quae!");
+
+echo $forest->render();
+echo $forest->print();
+
+$nationalPark = new card("https://source.unsplash.com/700x455/?nature", "National Park", "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut eum similique repellat a laborum, rerum voluptates ipsam eos quo tempore iusto dolore modi dolorum in pariatur. Incidunt repellendus praesentium quae!");
+
+echo $nationalPark->render();
+echo $nationalPark->print();
+
+
+?>
